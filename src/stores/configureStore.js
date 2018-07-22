f9f9a82c9d5b1d1f3a07fcb72746176638835860
Wayne Duran <asartalo@gmail.com>
2018-07-22 18:11:53 +0800

    Temporary localStorage persistence for redux store

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,16 +1,23 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers/index';
+import { save, load } from 'redux-localstorage-simple'
 
+import rootReducer from '../reducers/index';
 import devMiddlewares from './devMiddlewares';
 
 const middlewares = [
 	thunk
 ].concat(devMiddlewares);
+middlewares.push(save());
 
-const store = createStore(
+const createStoreWithMiddleware
+	= applyMiddleware(
+			save() // Saving done here
+	)(createStore);
+
+const store = createStoreWithMiddleware(
 	rootReducer,
-	applyMiddleware(...middlewares)
+	load()
 );
 
 export default store;
